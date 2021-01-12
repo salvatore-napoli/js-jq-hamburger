025be546c01e64acf9f227a1089b5f22fad71ec7
Salvatore Napoli <snapoli1998@gmail.com>
2021-01-12 14:37:25 +0100

    Mostrare e nascondere menu

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Mostrare il menu
+$('.fas.fa-bars').click(function() {
+  $('.hamburger-menu').show();
+});
+
+// Chiudere il menu
+$('.close').click(function() {
+  $('.hamburger-menu').hide();
+});
